435f679e9b9ba17621f66e6c361c7d1e1fa44edb
eldutche <eldutche.canal@gmail.com>
2021-08-23 07:50:58 -0300

    POO Java

---
+++ aula4/Aula04.java
@@ -0,0 +1,10 @@
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", 0.4, true, "Amarelo");
+        c1.status();
+
+        Caneta c2 = new Caneta("KKK", 1.5, false, "Laranja");
+        c2.status();
+
+    }
+}
